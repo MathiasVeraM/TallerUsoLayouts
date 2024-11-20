80cab75ab2125846f245e583576968a710163435
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-11-20 12:51:42 -0500

    Se creo el Grid Layout, con el ultimo cuadro que lleva a Stack Layout

--- TallerUsoLayouts/GridLayout.xaml.cs
+++ TallerUsoLayouts/GridLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class GridLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void StackLayout_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new StackLayout());
+    }
 }
