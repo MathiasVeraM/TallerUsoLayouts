850f7cae2feb896061591103161a1bcc745a139d
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-11-20 13:07:40 -0500

    Se creo la pantalla StackLayout y se añadieron colores para mejor estética

--- TallerUsoLayouts/StackLayout.xaml.cs
+++ TallerUsoLayouts/StackLayout.xaml.cs
@@ -6,4 +6,9 @@ public partial class StackLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void FlexLayoutButton_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new FlexLayout());
+    }
 }
